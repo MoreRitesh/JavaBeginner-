9d14d89bf506a7e67e753d18e4d99cd7f0bf9074
Ritesh More <133583162+MoreRitesh@users.noreply.github.com>
2025-02-18 08:47:18 +0530

    Add files via upload

---
+++ Pattern11.java
@@ -0,0 +1,26 @@
+class Pattern11
+{
+	public static void main(String[] args) 
+	{
+		int a=1;
+		char b='a';
+		for (int i=1;i<=5 ;i++ )
+		{
+			
+			for (int j=1;j<=i ;j++ )
+			{
+				if (i%2==0)
+				{
+					System.out.print(a++ +" ");
+				}
+				else
+					System.out.print(b
+					++ +" ");
+				
+
+			}
+			System.out.println();
+		}
+		
+	}
+}
