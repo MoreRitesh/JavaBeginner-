a71edc1a369a4dc348034994857fcea868640f80
Ritesh More <133583162+MoreRitesh@users.noreply.github.com>
2025-02-23 21:49:17 +0530

    Add files via upload

--- Numbers.java
+++ Numbers.java
@@ -1,13 +1,25 @@
-class Numbers
-{
-	public static void main(String[] args) 
-	{
-		int num=0;
-		while (num<=100)
-		{
-			System.out.println(num);
-			num++;
-		}
-		
-	}
+package com.kunal.properties.polymorphism;
+
+public class Numbers {
+
+    double sum(double a, int b) {
+        return a + b;
+    }
+
+    double sum(int a, int b) {
+        return a + b;
+    }
+
+    int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static void main(String[] args) {
+        Numbers obj = new Numbers();
+
+        obj.sum(2, 3);
+        obj.sum(1, 3, 7);
+
+//        obj.sum(4, 5, 6, 8);
+    }
 }
